a2da68e867af649d9367286e102faa288e319b60
Lachlan Houston <LachlanHouston111@gmail.com>
2025-09-03 14:05:35 +0200

    cookiecutter setup

---
+++ src/graph_ml/train.py
@@ -0,0 +1,10 @@
+from graph_ml.model import Model
+from graph_ml.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
